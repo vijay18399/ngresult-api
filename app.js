cf6e741b2a931641ba895a7ed1784fc555c512f0
vijay18399 <vijayreddy18399@gmail.com>
2022-05-20 13:29:47 +0530

    set headers for cors errors

--- app.js
+++ app.js
@@ -8,12 +8,9 @@ var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "content-type");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Accept");
   next();
 });
 app.use("/", resultRoute);
