1c2378bca661f5150e6496719f574567d0410e66
vijay18399 <vijayreddy18399@gmail.com>
2022-05-20 14:23:34 +0530

    order changed

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const resultRoute = require("./routes/result.route");
 var port = process.env.PORT || 3000;
 const cors = require("cors");
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use("/", resultRoute);
 
 app.use((error, req, res, next) => {
