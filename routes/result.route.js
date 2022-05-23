16f932a7ce0610e413264bbb994886d5f724e689
vijay18399 <vijayreddy18399@gmail.com>
2022-05-23 09:39:24 +0530

    using connect-timeout

--- routes/result.route.js
+++ routes/result.route.js
@@ -4,13 +4,14 @@ const ResultController = require("../controllers/result.controller");
 const upload = require("../middlewares/upload");
 var timeout = require("connect-timeout");
 function haltOnTimedout(req, res, next) {
+  console.log(req.timedout);
   if (!req.timedout) next();
 }
 router.get("/results", ResultController.getResults);
 router.get("/result/:resultLink", ResultController.getResult);
 router.post(
   "/result",
-  timeout("55s"),
+  timeout("65s"),
   haltOnTimedout,
   upload.single("pdf"),
   ResultController.postResult
