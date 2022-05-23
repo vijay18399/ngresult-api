49d35a2236dcb0062ca5ecab7dcef7f7baa214ea
vijay18399 <vijayreddy18399@gmail.com>
2022-05-23 10:37:17 +0530

    setTimeout to 1min

--- routes/result.route.js
+++ routes/result.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const ResultController = require("../controllers/result.controller");
 const upload = require("../middlewares/upload");
-
+var timeout = require("connect-timeout");
 router.get("/results", ResultController.getResults);
 router.get("/result/:resultLink", ResultController.getResult);
 router.post(
